fc88ee859d80a7100476e927a3ff39de9b9917ea
Nanchen Yang <ncyang@thoughtworks.com>
2017-11-27 17:20:54 +0800

    [feature] show forecast weather in xian.

--- app/src/main/java/nanchen/weather/ui/activities/MainActivity.kt
+++ app/src/main/java/nanchen/weather/ui/activities/MainActivity.kt
@@ -5,23 +5,26 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
-import android.widget.TextView
 import android.widget.Toast
+import kotlinx.coroutines.experimental.android.UI
+import kotlinx.coroutines.experimental.async
 import nanchen.weather.R
 import others.Person
 import nanchen.weather.domain.model.Forecast
 import nanchen.weather.domain.commands.RequestForecastCommand
 import nanchen.weather.ui.adapters.ForecastListAdapter
-import nanchen.weather.data.remote.Request
+import nanchen.weather.domain.model.ForecastList
+import org.jetbrains.anko.coroutines.experimental.bg
 import org.jetbrains.anko.find
-import org.jetbrains.anko.longToast
 
 class MainActivity : AppCompatActivity() {
+    lateinit var forecastList : RecyclerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val forecastList: RecyclerView = find(R.id.forecast_list)
-        async(forecastList)
+        forecastList = find(R.id.forecast_list)
+        forecastList.layoutManager = LinearLayoutManager(this)
+        loadForecast()
     }
 
     fun toast(message: String, length: Int = Toast.LENGTH_SHORT) {
@@ -50,25 +53,17 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    fun async(forecastList: RecyclerView) {
-        val result = RequestForecastCommand("94043").execute();
-        runOnUiThread {
-            forecastList.adapter = ForecastListAdapter(result, object : ForecastListAdapter.OnItemClickListener {
-                override fun invoke(forecast: Forecast) {
-                    toast(forecast.date)
-                }
-            })
+    private fun loadForecast() = async(UI) {
+        val result = bg { RequestForecastCommand("Xian,cn").execute() }
+        updateUI(result.await(), forecastList)
+    }
 
-            fun async(url: String) {
-                Request(url).run()
-                runOnUiThread {
-                    longToast("Request performed")
-                }
+    private fun updateUI(weekForecast: ForecastList, forecastListView: RecyclerView) {
+        val adapter = ForecastListAdapter(weekForecast, object : ForecastListAdapter.OnItemClickListener {
+            override fun invoke(forecast: Forecast) {
+                toast(forecast.date)
             }
-
-            fun Context.toast(message: String, length: Int = Toast.LENGTH_SHORT) {
-                Toast.makeText(this, message, length).show()
-            }
-        }
+        })
+        forecastListView.adapter = adapter
     }
 }
