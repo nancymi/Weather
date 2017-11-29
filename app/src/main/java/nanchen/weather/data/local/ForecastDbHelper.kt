0991b2e875a9b4acab93dda50f3c5e77c02ac0e0
Nanchen Yang <ncyang@thoughtworks.com>
2017-11-29 20:36:49 +0800

    [feature] defined db classes

---
+++ app/src/main/java/nanchen/weather/data/local/ForecastDbHelper.kt
@@ -0,0 +1,28 @@
+package nanchen.weather.data.local
+
+import android.database.sqlite.SQLiteDatabase
+import nanchen.weather.App
+import org.jetbrains.anko.db.*
+
+class ForecastDbHelper() : ManagedSQLiteOpenHelper(App.instance,
+        ForecastDbHelper.DB_NAME, null, ForecastDbHelper.DB_VERSION) {
+
+    companion object {
+        val DB_NAME = "forecast.db"
+        val DB_VERSION = 1
+        val instance: ForecastDbHelper by lazy { ForecastDbHelper() }
+    }
+
+    override fun onCreate(p0: SQLiteDatabase?) {
+        db.createTable(CityForecastTable.NAME, true,
+                Pair(CityForecastTable.ID, INTEGER + PRIMARY_KEY),
+                Pair(CityForecastTable.CITY, TEXT),
+                Pair(CityForecastTable.COUNTRY, TEXT))
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onUpgrade(p0: SQLiteDatabase?, p1: Int, p2: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
